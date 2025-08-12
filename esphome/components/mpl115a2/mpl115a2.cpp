8505f97e47d34ffa3627569206b7e48b9ef97119
Bean Man <86612985+BeanMan1408@users.noreply.github.com>
2025-08-12 17:15:23 -0400

    Update mpl115a2.cpp

--- esphome/components/mpl115a2/mpl115a2.cpp
+++ esphome/components/mpl115a2/mpl115a2.cpp
@@ -47,33 +47,31 @@ void MPL115A2Component::dump_config() {
 }
 
 void MPL115A2Component::update() {
-  
+  uint8_t cmd[2] = {MPL115A2_REGISTER_STARTCONVERSION, 0};
+  uint8_t buffer[4];
+  this->write(cmd, 2);
   // Wait a bit for the conversion to complete (3ms max)
-  this->set_timeout(5, [this]() {
-    uint8_t cmd[2] = {MPL115A2_REGISTER_STARTCONVERSION, 0};
-    uint8_t buffer[4];
-    this->write(cmd, 2);
-    
-    cmd[0] = MPL115A2_REGISTER_PRESSURE_MSB;
-    this->write(cmd, 1);
-    this->read(buffer, 4);
+  delay(5);
 
-    uint16_t pressure_in = (((uint16_t)buffer[0] << 8) | buffer[1]) >> 6;
-    uint16_t temp_in = (((uint16_t)buffer[2] << 8) | buffer[3]) >> 6;
+  cmd[0] = MPL115A2_REGISTER_PRESSURE_MSB;
+  this->write(cmd, 1);
+  this->read(buffer, 4);
 
-    // See datasheet p.6 for evaluation sequence
-    float pressure_comp = mpl115a2_a0_ + (mpl115a2_b1_ + mpl115a2_c12_ * temp_in) * pressure_in + mpl115a2_b2_ * temp_in;
+  uint16_t pressure_in = (((uint16_t)buffer[0] << 8) | buffer[1]) >> 6;
+  uint16_t temp_in = (((uint16_t)buffer[2] << 8) | buffer[3]) >> 6;
 
-    float pressure_out = ((65.0f / 1023.0f) * pressure_comp) + 50.0f;
-    if (this->pressure_ != nullptr)
-      this->pressure_->publish_state(pressure_out);
+  // See datasheet p.6 for evaluation sequence
+  float pressure_comp = mpl115a2_a0_ + (mpl115a2_b1_ + mpl115a2_c12_ * temp_in) * pressure_in + mpl115a2_b2_ * temp_in;
 
-    float temperature_out = ((float)temp_in - 498.0f) / -5.35f + 25.0f;
-    if (this->temperature_ != nullptr)
-      this->temperature_->publish_state(temperature_out);
+  float pressure_out = ((65.0f / 1023.0f) * pressure_comp) + 50.0f;
+  if (this->pressure_ != nullptr)
+    this->pressure_->publish_state(pressure_out);
 
-    ESP_LOGD(TAG, "Got Temperature=%.1f Pressure=%.1f", temperature_out, pressure_out);
-  });
+  float temperature_out = ((float)temp_in - 498.0f) / -5.35f + 25.0f;
+  if (this->temperature_ != nullptr)
+    this->temperature_->publish_state(temperature_out);
+
+  ESP_LOGD(TAG, "Got Temperature=%.1f Pressure=%.1f", temperature_out, pressure_out);
 }
 
 }  // namespace mpl115a2
